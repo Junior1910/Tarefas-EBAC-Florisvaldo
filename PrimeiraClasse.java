73121e9e5a248326e82b94bb76ffecb99c0d1e1d
Florisvaldo <juniortcj10012@hotmail.com>
2024-01-13 21:17:59 -0300

    Impressão de uma saudação e dois nomes

---
+++ PrimeiraClasse.java
@@ -0,0 +1,11 @@
+package PrimeiroPacote;
+import java.util.Scanner;
+public class PrimeiraClasse {
+    public static void main(String[] args) {
+        String nome1, nome2;
+        nome1 = "Adelaide";
+        nome2 = "Florisvaldo";
+        System.out.print("Hello " + nome1 + " and " + nome2+".");
+
+    }
+}
